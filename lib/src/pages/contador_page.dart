cc9d22dcdd9352f9ae510eb157cbd8c87b5d8432
miler-vrg <milervargas09@gmail.com>
2021-10-26 19:44:21 -0500

    Se le agregaron tres botones al proyecto

--- lib/src/pages/contador_page.dart
+++ lib/src/pages/contador_page.dart
@@ -6,37 +6,67 @@ class ContadorPage extends StatefulWidget {
 }
 
 class _ContadorPageState extends State<ContadorPage> {
-  final _txtStyle = new TextStyle(fontSize: 20);
+  final _txtStyle =
+      new TextStyle(fontSize: 20, color: Color.fromRGBO(255, 245, 21, 1));
   int _conteo = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Contador'),
-        centerTitle: true,
-        elevation: 5.5,
-      ),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center, //Ubicación
-        children: <Widget>[
-          Text(
-            'Numero de pusaciones:',
-            style: _txtStyle,
-          ),
-          Text('\nN°: $_conteo', style: _txtStyle),
-        ],
-      )),
-      backgroundColor: Color.fromRGBO(248, 239, 199, 1),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _conteo++;
-          setState(() {});
-        }, //oyente
-        child: Icon(Icons.add),
-      ),
-      // floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat, //location button
+        appBar: AppBar(
+          title: Text('Contador'),
+          centerTitle: true,
+          elevation: 5.5,
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, //Ubicación
+          children: <Widget>[
+            Text(
+              'Numero de pulsaciones:',
+              style: _txtStyle,
+            ),
+            Text('\nN°: $_conteo', style: _txtStyle),
+          ],
+        )),
+        backgroundColor: Color.fromRGBO(0, 0, 33, 20),
+        floatingActionButton: _crearBotones()
+        // floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat, //location button
+        );
+  }
+
+  Widget _crearBotones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        SizedBox(width: 30.0), //Separaciones
+        FloatingActionButton(
+          onPressed: _limpiar,
+          child: Icon(Icons.exposure_zero),
+        ),
+        Expanded(child: SizedBox()), //Separaciones
+        FloatingActionButton(
+          onPressed: _disminuir,
+          child: Icon(Icons.remove),
+        ),
+        SizedBox(width: 30.0), //Separaciones
+        FloatingActionButton(
+          onPressed: _aumentar,
+          child: Icon(Icons.add),
+        ),
+      ],
     );
+  }
+
+  void _disminuir() {
+    setState(() => _conteo--);
+  }
+
+  void _aumentar() {
+    setState(() => _conteo++);
+  }
+
+  void _limpiar() {
+    setState(() => _conteo = 0);
   }
 }
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 
 //------------------- Widget ------------
-//            StatelessWidget
+//                 StatelessWidget
 
 class HomePage extends StatelessWidget {
   final txtStyle = new TextStyle(fontSize: 20);
